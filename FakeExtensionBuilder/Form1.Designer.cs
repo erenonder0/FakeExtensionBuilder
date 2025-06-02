8bfb27bf738e6dc91226806a4e8d423a3c3723d6
erenonder0 <eerenonder0@gmail.com>
2025-06-02 11:10:58 +0300

    first commit

---
+++ FakeExtensionBuilder/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FakeExtensionBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(607, 177);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Dosya Seç";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonFace;
+            button2.FlatAppearance.MouseOverBackColor = SystemColors.AppWorkspace;
+            button2.Font = new Font("Verdana", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(191, 256);
+            button2.Name = "button2";
+            button2.Size = new Size(446, 52);
+            button2.TabIndex = 1;
+            button2.Text = "Fake Extension İle Yeniden Adlandır";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(135, 177);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(456, 32);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(135, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Dosya:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(707, 405);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(35, 33);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Cursor = Cursors.Hand;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(748, 405);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(35, 33);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Cursor = Cursors.Hand;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(666, 405);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(35, 33);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Braille Pattern Blank";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+    }
+}
---
+++ FakeExtensionBuilder/Form1.cs
@@ -0,0 +1,124 @@
+﻿namespace FakeExtensionBuilder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Bir dosya seçin";
+            openFileDialog1.Filter = "Tüm Dosyalar|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openFileDialog1.FileName;
+                textBox1.Text = selectedFile;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string originalPath = textBox1.Text;
+            string unicode = "\u2800";
+
+            if (string.IsNullOrWhiteSpace(originalPath))
+            {
+                MessageBox.Show("Lütfen dosya seçin!");
+                return;
+            }
+
+            string inputName = Microsoft.VisualBasic.Interaction.InputBox("Yeni dosya adını girin:", "Dosya Adı", "yeni_isim");
+
+            if (string.IsNullOrWhiteSpace(inputName))
+            {
+                MessageBox.Show("Dosya adı girilmedi.");
+                return;
+            }
+
+            string directory = Path.GetDirectoryName(originalPath);
+            string fileName = inputName;
+            string extension = Path.GetExtension(originalPath);
+
+            int baseNameLength = fileName.Length + extension.Length;
+            int baseFullPathLength = Path.Combine(directory, fileName + extension).Length;
+
+            int maxTotalPathLength = 250;
+            int remainingLength = maxTotalPathLength - baseFullPathLength;
+
+            if (remainingLength < 0)
+            {
+                MessageBox.Show("Klasör yolu çok uzun, unicode karakter eklenemez.");
+                return;
+            }
+
+            string ekle = string.Concat(Enumerable.Repeat(unicode, remainingLength));
+            string newFileName = fileName + ekle + extension;
+            string newPath = Path.Combine(directory, newFileName);
+
+            try
+            {
+                if (File.Exists(newPath))
+                {
+                    MessageBox.Show("Aynı isimde bir dosya zaten var.");
+                    return;
+                }
+
+                File.Copy(originalPath, newPath);
+                MessageBox.Show("Dosya yeniden adlandırıldı:\n" + newFileName);
+                textBox1.Clear();
+            }
+            catch (IOException ioEx)
+            {
+                MessageBox.Show("Giriş/Çıkış hatası:\n" + ioEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Beklenmeyen hata:\n" + ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://linktr.ee/eren.onder",
+                UseShellExecute = true
+            });
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://www.linkedin.com/in/erenonder0/",
+                UseShellExecute = true
+            });
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://erenonder0.github.io/",
+                UseShellExecute = true
+            });
+        }
+    }
+}
